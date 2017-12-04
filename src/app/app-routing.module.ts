68361a55c6f1d7b4a416ddeffd242b45f128346c
HankXu <527665221@qq.com>
2017-12-04 19:54:39 +0800

    修正通配路由配置

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order-module/order.component';
 import { LoginComponent } from './login-module/login.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent },
     {path: '', redirectTo: '/eating', pathMatch: 'full'},
-    {path: '**', component: OrderComponent}
+    {path: '**', redirectTo: '/eating', pathMatch: 'full'}
 ];
 
 @NgModule({
